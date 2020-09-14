efce43a20ee45ca9f3e0f51bff25c5cbe742ddfb
lzy <mlzy7318@163.com>
2020-09-14 16:02:45 +0800

    新建项目

---
+++ test/src/page/one.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="one">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style lang="">
+    
+</style>
